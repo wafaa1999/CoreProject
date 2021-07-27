bb53d79a36f26dfa318a4d223363321366b413ac
Wafaa Daas <w.daas.1999@hotmail.com>
2021-07-27 05:07:44 +0300

    commitTwo

--- HandleReqests.py
+++ HandleReqests.py
@@ -29,20 +29,6 @@ def redirect_login():
     return redirect(url)
 
 
-@app.route("/firstTime", methods=['GET'])
-def firstTime():
-    response = []
-    idUser = request.args.get('idUser')
-    password = request.args.get('password')
-    req1 = req()
-    re = req1.get_dep(idUser,password)
-    row = dict(
-        stat=re
-    )
-    response.append(row)
-    return jsonify({'response': response})
-
-
 @app.route("/checkAndSendEmail", methods=['GET'])
 def redirect_checkAndSendEmail():
     email = request.args.get('email')
@@ -153,14 +139,6 @@ def saveMatOfDraft():
     )
     response.append(row)
     return jsonify({'response': response})
-
-
-@app.route("/getAllIsn", methods=['GET'])
-def getAllIsn():
-    idDep = request.args.get('idDep')
-    url = "https://virtual-grad.herokuapp.com/getAllIsn"
-    url += "?idDep=" + idDep
-    return redirect(url)
 
 
 @app.route("/deleteRoomFromDep", methods=['GET'])
